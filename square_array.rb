4873edad04e0b15d9f0f958bd9f0e85a6f15ddda
Waverley Leung <waverley.nyc@gmail.com>
2020-03-22 21:28:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   squared_array = []
-  numbers.each do |num|
-    squared_array << num ** 2
+  numbers.each do |number|
+    squared_array << number ** 2
   end
   squared_array
 end
